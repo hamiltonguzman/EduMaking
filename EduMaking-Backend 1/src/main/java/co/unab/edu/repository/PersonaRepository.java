8f456098e33ad19b41749b34192895e461c6d8dd
hamiltonguzman <89691481+hamiltonguzman@users.noreply.github.com>
2021-10-05 21:25:30 -0500

    error en test
    error en test

--- EduMaking-Backend 1/src/main/java/co/unab/edu/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.unab.edu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.unab.edu.models.entity.Persona;
-
-public interface PersonaRepository extends JpaRepository<Persona, Integer> {
-
-}
